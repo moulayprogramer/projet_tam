d8ad34766ee0c86a72a08b4bc28361d665af4d9b
moulayprogramer <30800414+moulayprogramer@users.noreply.github.com>
2017-08-23 16:03:09 +0100

    23-08-2017/ 16:03

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Cv;
+use App\Demend_de_emp;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -32,7 +34,20 @@ class AdminController extends Controller
     {
         $current_date = Carbon::now()->format('Y-m-d');
         $Registered_today_count = User::where('created_at' ,'like' ,$current_date.'%')->count();
-        $arr = Array('user' => Auth::user(),'Registered_today_count'=> $Registered_today_count, 'current_date'=> $current_date);
+        $aamel_today_count = Cv::where('created_at' ,'like' ,$current_date.'%')->count();
+        $wadifa_today_count = Demend_de_emp::where('created_at' ,'like' ,$current_date.'%')->count();
+        $Registered_count = User::all()->count();
+        $aamel_count = Cv::all()->count();
+        $wadifa_count = Demend_de_emp::all()->count();
+        $arr = Array(
+            'user' => Auth::user(),
+            'Registered_today_count'=> $Registered_today_count,
+            'aamel_today_count' => $aamel_today_count,
+            'wadifa_today_count' => $wadifa_today_count,
+            'Registered_count' => $Registered_count,
+            'aamel_count' => $aamel_count,
+            'wadifa_count' => $wadifa_count,
+        );
         return view('admin.home',$arr);
     }
     public function show_registered_today(){
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin_layouts')
 @section('admin_dash')
-    <div class="col-lg-3 col-md-6">
+    <div class="col-lg-4 col-xs-12 col-md-6">
         <div class="panel panel-green">
             <div class="panel-heading">
                 <div class="row">
@@ -19,70 +19,147 @@
             </a>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
+    <div class="col-lg-4 col-xs-12 col-md-6">
+        <div class="panel panel-yellow">
             <div class="panel-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-support fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                        <div class="huge">13</div>
-                        <div>Support Tickets!</div>
+                        <div class="huge">{{ $wadifa_today_count }}</div>
+                        <div>الوظائف المطلوبة اليوم</div>
                     </div>
                 </div>
             </div>
             <a href="#">
                 <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
+                    <span class="pull-left">القوائم</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
+    <div class="col-lg-4 col-xs-12 col-md-6">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-support fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                        <div class="huge">13</div>
-                        <div>Support Tickets!</div>
+                        <div class="huge">{{ $aamel_today_count }}</div>
+                        <div>العمال المسجلين اليوم</div>
                     </div>
                 </div>
             </div>
             <a href="#">
                 <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
+                    <span class="pull-left">القوائم</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
+
+    <div class="col-lg-4 col-xs-12 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-12 text-right">
+                        <div class="huge">{{ $Registered_count }} </div>
+                        <div>المسجلين في الموقع</div>
+                    </div>
+                </div>
+            </div>
+            <a href="/register_today_list">
+                <div class="panel-footer">
+                    <span class="pull-left">القوائم</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-4 col-xs-12 col-md-6">
+        <div class="panel panel-yellow">
             <div class="panel-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-support fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                        <div class="huge">13</div>
-                        <div>Support Tickets!</div>
+                        <div class="huge">{{ $wadifa_count }}</div>
+                        <div>الوظائف المطلوبة</div>
                     </div>
                 </div>
             </div>
             <a href="#">
                 <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
+                    <span class="pull-left">القوائم</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
         </div>
     </div>
+    <div class="col-lg-4 col-xs-12 col-md-6">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-support fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class="huge">{{ $aamel_count }}</div>
+                        <div>العمال المسجلين</div>
+                    </div>
+                </div>
+            </div>
+            <a href="#">
+                <div class="panel-footer">
+                    <span class="pull-left">القوائم</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-12 col-xs-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <span class="text-center">رسائل البريد</span>
+            <div class="pull-left btn-default btn-sm"><i class="glyphicon glyphicon-refresh"></i></div>
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th> البريد</th>
+                            <th> التفاصيل</th>
+                        </tr>
+                        </thead>
+                       <tbody>
+
+                            <tr>
+                                <td>n</td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-link">>></a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+
 @endsection
